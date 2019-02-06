6cef27bb01538e0cb1a3a7dcdae5143e1bc03a28
lehela <leif.lange@gmail.com>
2019-02-06 11:15:57 +0000

    dynamic user directory

--- node-skat.sh
+++ node-skat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-node-red --port 1881 --settings /home/pi/.node-red-skatliste/settings.js --title Skatliste --userDir /home/pi/.node-red-skatliste/ &
+node-red --port 1881 --settings /home/$USER/.node-red-skatliste/settings.js --title Skatliste --userDir /home/$USER/.node-red-skatliste/ &
 sudo netstat -tulpn|grep 1881
