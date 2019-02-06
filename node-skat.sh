269411d32338c0ed3b59c181fe256ec173037a15
lehela <leif.lange@gmail.com>
2019-02-06 10:57:48 +0000

    changed bash start command

--- node-skat.sh
+++ node-skat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-node-red --port 1881 --settings /home/pi/.node-red-skat/settings.js --title Skat --userDir /home/pi/.node-red-skat/ &
+node-red --port 1881 --settings /home/pi/.node-red-skatliste/settings.js --title Skatliste --userDir /home/pi/.node-red-skatliste/ &
 sudo netstat -tulpn|grep 1881
